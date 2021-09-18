863ba87bcf42d62bbb1c7d5e00dd3ddddf1c6b11
Felipe Slaughter-Quintero <fslauq@gmail.com>
2021-09-18 09:52:56 -0400

    working on restricted middleware for auth and recipes..Heroku push

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const server = express();
 
 // building out routes
+const restrict = require('./auth/auth-middleware.js')
 const authRouter = require('./auth/auth-router.js');
 const recipesRouter = require('./recipe/recipe-router.js');
 
@@ -12,8 +13,8 @@ server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
-server.use('/api/recipes', recipesRouter);
-server.use('/api/auth', authRouter);
+server.use('/api/recipes', restrict, recipesRouter);
+server.use('/api/auth', restrict, authRouter);
 
 server.get('/', (req, res) => {
   res.json({ api: 'API is UP!' });
